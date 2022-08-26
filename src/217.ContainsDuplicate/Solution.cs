07f975d3d92b60fd4d02b6c4774a20a7282bbeea
Alain Licona <conexionadsl@gmail.com>
2022-08-26 17:01:39 +0200

    Solve problem 217

---
+++ src/217.ContainsDuplicate/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+/*
+Given an integer array nums, 
+return true if any value appears at least twice in the array, 
+and return false if every element is distinct.
+*/
+namespace _217.ContainsDuplicate
+{
+    public class Solution
+    {
+        
+        public bool ContainsDuplicate(int[] nums)
+        {
+            var dictionary = new Dictionary<int, int>();
+            foreach (int num in nums)
+            {
+                if (!dictionary.TryAdd(num, num))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ tests/217.ContainsDuplicate.Tests/SolutionTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+
+namespace _217.ContainsDuplicate.Tests
+{
+    [TestFixture]
+    public class ContainsDuplicateTests
+    {
+        private Solution _sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _sut = new Solution();
+        }
+
+        [TestCase(new int[] {1,2,3,1}, true)]
+        [TestCase(new int[] {5,2,3,1,5}, true)]
+        [TestCase(new int[] {1,1,1,3,3,4,3,2,4,2}, true)]
+        [TestCase(new int[] {1,2,3,4}, false)]
+        [TestCase(new int[] {5,7,9,1,3}, false)]
+        [TestCase(new int[] {11,13,15}, false)]
+        public void Should_returnTrueWhenDuplicate(int[] nums, bool expected)
+        {
+            // Arrange / Act
+            var result = _sut.ContainsDuplicate(nums);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
